3fa90e8bf0374c4ab4a7f20ca200f5e3162c85c3
Sergei Mikhailovskii <33265881+SergeiMikhailovskii@users.noreply.github.com>
2019-02-19 18:55:42 +0300

    Update Task 1.java

--- Tasks 1/Task 1.java
+++ Tasks 1/Task 1.java
@@ -1,6 +1,6 @@
 /*
-*Разработайте программу, которая проверяет, что числа a, b и c различны(одинаковы).
-*/
+ *Разработайте программу, которая проверяет, что числа a, b и c различны(одинаковы).
+ */
 
 package com.company;
 
@@ -9,23 +9,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    int a, b, c;
+        int a, b, c;
         a = enterTheValue();
         b = enterTheValue();
         c = enterTheValue();
-        if (a==b){
-            if (b ==c){
+        if (a == b) {
+            if (b == c) {
                 System.out.println("All numbers are the same");
-            }
+            } 
             else {
                 System.out.println("2 numbers are the same");
             }
-        }
-        else{
-            if (b == c){
+        } 
+        else {
+            if (b == c) {
                 System.out.println("2 numbers are the same");
-            }
-            else if (a ==c){
+            } 
+            else if (a == c) {
                 System.out.println("2 numbers are the same");
             }
             else {
@@ -34,19 +34,21 @@ public class Main {
         }
     }
 
-    private static int enterTheValue(){
-        int value;
+    private static int enterTheValue() {
+        int value = 0;
+        boolean isInputCorrect = false;
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        do{
             System.out.println("Enter the value of the number");
-            if (scanner.hasNextInt()){
+            if (scanner.hasNextInt()) {
                 value = scanner.nextInt();
-                return value;
-            }
+                isInputCorrect = true;
+            } 
             else {
                 System.out.println("Wrong input");
                 scanner.nextLine();
             }
-        }
+        }while (!isInputCorrect);
+        return value;
     }
 }
