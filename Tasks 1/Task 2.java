b991c0068090c1ba586f1995d2f18b2257c6735a
Sergei Mikhailovskii <33265881+SergeiMikhailovskii@users.noreply.github.com>
2019-02-19 19:01:44 +0300

    Update Task 2.java

--- Tasks 1/Task 2.java
+++ Tasks 1/Task 2.java
@@ -1,6 +1,6 @@
 /*
-    *Масса динозавра задаётся в килограммах. Разработайте программу, которая вычисляет, сколько это миллиграмм, грамм и тонн.
-*/
+ *Масса динозавра задаётся в килограммах. Разработайте программу, которая вычисляет, сколько это миллиграмм, грамм и тонн.
+ */
 
 package com.company;
 
@@ -12,65 +12,76 @@ public class Main {
     public static void main(String[] args) {
         int weight = enterWeight();
         double convertedWeight = -1;
-        String measure="";
-        switch (menu()){
-            case 1:
-                convertedWeight = weight*1000000;
-                measure = "mg";
-                break;
-            case 2:
-                convertedWeight=weight*1000;
-                measure = "g";
-                break;
-            case 3:
-                convertedWeight=(double) weight/1000;
-                measure = "t";
-                break;
-            default:
-                System.out.println("Wrong input");
-                break;
-        }
-        if (convertedWeight!=-1) {
-            System.out.println(weight+" kg is " +convertedWeight+" "+measure);
-        }
+        String measure = "";
+        int choice;
+        do {
+            choice = menu();
+            switch (choice) {
+                case 1:
+                    convertedWeight = weight * 1000000;
+                    measure = "mg";
+                    break;
+                case 2:
+                    convertedWeight = weight * 1000;
+                    measure = "g";
+                    break;
+                case 3:
+                    convertedWeight = (double) weight / 1000;
+                    measure = "t";
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Wrong input");
+                    break;
+            }
+            if (convertedWeight != -1) {
+                System.out.println(weight + " kg is " + convertedWeight + " " + measure);
+            }
+        } while (choice != 0);
     }
 
-    private static int enterWeight(){
+    private static int enterWeight() {
         Scanner scanner = new Scanner(System.in);
         int weight;
-        while (true){
+        while (true) {
             System.out.println("Enter the weight");
-            if (scanner.hasNextInt()){
+            if (scanner.hasNextInt()) {
                 weight = scanner.nextInt();
-                if (weight>0) {
+                if (weight > 0) {
                     return weight;
-                }
+                } 
                 else {
                     System.out.println("Weight should be more than 0");
                 }
-            }
-            else{
+            } 
+            else {
                 System.out.println("Wrong input, Please try again");
                 scanner.nextLine();
             }
         }
     }
 
-    private static int menu(){
+    private static int menu() {
         Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        boolean isInputCorrect = false;
         System.out.println("Enter the variant you need to convert:");
         System.out.println("1 - milligrams");
         System.out.println("2 - grams");
         System.out.println("3 - tones");
-        while (true){
-            if (scanner.hasNextInt()){
-                return scanner.nextInt();
-            }
+        System.out.println("0 - exit");
+        do {
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+                isInputCorrect = true;
+            } 
             else {
                 System.out.println("Wrong input, Please try again");
                 scanner.nextInt();
             }
-        }
+        } while (!isInputCorrect);
+        return number;
     }
 
 
